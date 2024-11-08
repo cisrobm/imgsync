a1718a519dcf3bde169743e41bd60dd5b12a36bd
cisrobm <mos6502@gmx.de>
2024-11-08 11:40:10 +0100

    Update imgsync.sh

--- imgsync.sh
+++ imgsync.sh
@@ -3,6 +3,9 @@
 # size of boot partition in MiB (default is 512)
 bootsize=512
 
+bootmountpoint=/mnt/bootfs
+rootmountpoint=/mnt/rootfs
+
 if [ -z "${1}" ]; then
 	echo No output filename specified
 	exit
@@ -41,16 +44,16 @@ losetup -P loop0 $outfile
 # create boot filesystem, mount the partition and copy the files
 mkfs.vfat -F32 /dev/loop0p1
 fatlabel /dev/loop0p1 bootfs
-mkdir -p /mnt/bootfs
-mount /dev/loop0p1 /mnt/bootfs
-rsync $rsync_options /boot/firmware/ /mnt/bootfs
+mkdir -p $bootmountpoint
+mount /dev/loop0p1 $bootmountpoint
+rsync $rsync_options /boot/firmware/ $bootmountpoint
 
 # create root filesystem, mount the partition and copy the files
 mkfs.ext4 -m 0 /dev/loop0p2
 e2label /dev/loop0p2 rootfs
 
-mkdir -p /mnt/rootfs
-mount /dev/loop0p2 /mnt/rootfs
+mkdir -p $rootmountpoint
+mount /dev/loop0p2 $rootmountpoint
 echo "Base image created and mounted. Starting rsync ..."
 rsync $rsync_options --delete \
 			--exclude '/var/swap' \
@@ -64,34 +67,34 @@ rsync $rsync_options --delete \
 			--exclude 'lost\+found/*' \
 			--exclude '/mnt/*' \
    			--exclude '/var/swap' \
-		/ /mnt/rootfs
+		/ $rootmountpoint
 
 # populate /dev directory
-mknod /mnt/rootfs/dev/console c 5 1
-mknod /mnt/rootfs/dev/null c 1 3
-mknod /mnt/rootfs/dev/zero c 1 5
-mknod /mnt/rootfs/dev/full c 1 7
-mknod /mnt/rootfs/dev/ptmx c 5 2
-mknod /mnt/rootfs/dev/random c 1 8
-mknod /mnt/rootfs/dev/urandom c 1 9
-mknod /mnt/rootfs/dev/tty c 5 0
-mkdir -p /mnt/rootfs/dev/pts
-mkdir -p /mnt/rootfs/dev/shm
-ln -s /proc/self/fd /mnt/rootfs/dev/fd
-ln -s /proc/self/fd/0 /mnt/rootfs/dev/stdin
-ln -s /proc/self/fd/1 /mnt/rootfs/dev/stdout
-ln -s /proc/self/fd/2 /mnt/rootfs/dev/stderr
+mknod $rootmountpoint/dev/console c 5 1
+mknod $rootmountpoint/dev/null c 1 3
+mknod $rootmountpoint/dev/zero c 1 5
+mknod $rootmountpoint/dev/full c 1 7
+mknod $rootmountpoint/dev/ptmx c 5 2
+mknod $rootmountpoint/dev/random c 1 8
+mknod $rootmountpoint/dev/urandom c 1 9
+mknod $rootmountpoint/dev/tty c 5 0
+mkdir -p $rootmountpoint/dev/pts
+mkdir -p $rootmountpoint/dev/shm
+ln -s /proc/self/fd $rootmountpoint/dev/fd
+ln -s /proc/self/fd/0 $rootmountpoint/dev/stdin
+ln -s /proc/self/fd/1 $rootmountpoint/dev/stdout
+ln -s /proc/self/fd/2 $rootmountpoint/dev/stderr
 
 # update PARTUUIDs
 newuuid=`sfdisk -d $outfile | grep label-id | sed 's/label-id: 0x\(.*\)/\1/'`
-sed -i "s/PARTUUID=.*-02/PARTUUID=$newuuid-02/" /mnt/bootfs/cmdline.txt
-sed -i "s/PARTUUID=.*-01\(\s\+\/boot\/firmware\)/PARTUUID=$newuuid-01\1/" /mnt/rootfs/etc/fstab
-sed -i "s/PARTUUID=.*-02\(\s\+\/\s\+\)/PARTUUID=$newuuid-02\1/" /mnt/rootfs/etc/fstab
+sed -i "s/PARTUUID=.*-02/PARTUUID=$newuuid-02/" $bootmountpoint/cmdline.txt
+sed -i "s/PARTUUID=.*-01\(\s\+\/boot\/firmware\)/PARTUUID=$newuuid-01\1/" $rootmountpoint/etc/fstab
+sed -i "s/PARTUUID=.*-02\(\s\+\/\s\+\)/PARTUUID=$newuuid-02\1/" $rootmountpoint/etc/fstab
 
 # cleanup
-umount /mnt/rootfs
-umount /mnt/bootfs
-rmdir /mnt/bootfs
-rmdir /mnt/rootfs
+umount $rootmountpoint
+umount $bootmountpoint
+rmdir $bootmountpoint
+rmdir $rootmountpoint
 losetup -d /dev/loop0
 sync
